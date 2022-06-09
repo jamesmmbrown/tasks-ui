c167afd63b5702f4dd0806b15dfb456ed5868cf1
James Brown <James.Brown@verint.com>
2022-06-09 15:50:13 +0100

    Add basic admin page

---
+++ src/tasksadmin/TasksAdmin.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import TasksAdmin from "./TasksAdmin";
+
+describe('tasks admin page', () => {
+    it('displays tasks text', () => {
+        render(<TasksAdmin />);
+
+        expect(screen.getByText('Tasks')).toBeInTheDocument();
+    });
+});
---
+++ src/tasksadmin/TasksAdmin.tsx
@@ -0,0 +1,3 @@
+export default function TasksAdmin() {
+    return <p>Tasks</p>;
+}
