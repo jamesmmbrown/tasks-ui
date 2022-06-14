35f41acd5a0e42be2e018ce18e2a77ae338e1790
James Brown <jamesmmbrown@outlook.com>
2022-06-14 16:55:28 +0100

    Hardcode API base url

--- src/RestClient.ts
+++ src/RestClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const RestClient = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: "https://palm-tasks-api.herokuapp.com"
 });
 
 export default RestClient;
